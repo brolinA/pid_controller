bb425c7281cbf7f3ef349cfad79544d4503a19d9
Brolin A <brolin.a@botsync.co>
2021-07-04 16:27:28 +0530

    Updating functions with Anti-windup and output constraints.

--- include/pid_smd.h
+++ include/pid_smd.h
@@ -1,3 +1,13 @@
+/*********************************
+ *
+ * Software License Agreement 
+ *
+ *  Copyright (c) 2021. All rights reserved.
+ *
+ * Author: Brolin A 
+ *
+ *********************************/
+
 #ifndef PID_SMD_H
 #define PID_SMD_H
 
@@ -15,7 +25,7 @@ namespace pid_smd
 			pidSMDSystem(double kp_, double ki_, double kd_);
 
 			//destructor
-			~pidSMDSystem(){}
+			~pidSMDSystem() {}
 
 			void setGainValues(double new_kp, double new_ki, double new_kd);
 
@@ -25,9 +35,21 @@ namespace pid_smd
 
 			double getCutoffFrequency();
 
+			void setSamplingTime(double time_);
+
+			double getSamplingTime();
+
 			void setTimeConstant(double new_time_const);
 
 			double getTimeConstant();
+
+			void setIntegratorLimit(double min_val, double max_val);
+
+			void getIntegratorLimit(double &min_, double &max_);
+
+			void setSystemLimits(double min_, double max_);
+
+			void getSystemLimits(double &min_, double &max_);
 
 			double pidUpdate(double set_pt, double measurement);
 
@@ -37,7 +59,13 @@ namespace pid_smd
 
 			double tau; //time constant for low pass filter
 
-			double prev_error, prev_measurement;
+			double sampling_time;
+
+			double prev_error, prev_measurement, prev_integrator, prev_differentiator;
+
+			double max_int_limit, min_int_limit;
+
+			double system_max, system_min;
 	};
 }
 
--- src/pid_smd.cpp
+++ src/pid_smd.cpp
@@ -3,60 +3,124 @@
 namespace pid_smd
 {
 
-pidSMDSystem::pidSMDSystem()
-{
-	std::cout << "contructor called" << std::endl;
+pidSMDSystem::pidSMDSystem(){
+	std::cout << "[pidSMDSystem constructor] Class object created" << std::endl;
 }
 
 pidSMDSystem::pidSMDSystem(double kp_, double ki_, double kd_)
-							:kp(kp_), ki(ki_), kd(kd_)
-{
+							:kp(kp_), ki(ki_), kd(kd_){
 	this->tau = 0.0;
 	this->prev_error = 0.0;
 	this->prev_measurement = 0.0;
+	this->max_int_limit = 1.0;
+	this->min_int_limit = 0.0;
+	this->prev_integrator = 0.0;
+	this->prev_differentiator = 0.0;
+	this->sampling_time = 0.01; //in seconds
+	this->system_max = 1.0;
+	this->system_min = 0.0;
 }
 
-void pidSMDSystem::setGainValues(double new_kp, double new_ki, double new_kd)
-{
+void pidSMDSystem::setGainValues(double new_kp, double new_ki, double new_kd){
 	this->kp = new_kp;
 	this->ki = new_ki;
 	this->kd = new_kd;
 }
 
-void pidSMDSystem::getGainValues(double &kp_, double &ki_, double &kd_)
-{
+void pidSMDSystem::getGainValues(double &kp_, double &ki_, double &kd_){
 	kp_ = this->kp;
 	ki_ = this->ki;
 	kd_ = this->kd;
 }
 
-void pidSMDSystem::setCutoffFrequency(double freq)
-{
+void pidSMDSystem::setCutoffFrequency(double freq){
 	this->tau = 1/freq;
 }
 
-double pidSMDSystem::getCutoffFrequency()
-{
+double pidSMDSystem::getCutoffFrequency(){
 	return 1/tau;
 }
 
-void pidSMDSystem::setTimeConstant(double new_time_const)
-{
+void pidSMDSystem::setSamplingTime(double time_){
+	this->sampling_time = time_;
+}
+
+double pidSMDSystem::getSamplingTime(){
+	return this->sampling_time;
+}
+
+void pidSMDSystem::setTimeConstant(double new_time_const){
 	this->tau = new_time_const;
 }
 
-double pidSMDSystem::getTimeConstant()
-{
+double pidSMDSystem::getTimeConstant(){
 	return this->tau;
 }
 
-double pidSMDSystem::pidUpdate(double set_pt, double measurement)
-{
-	double controlled_out; //variable to return the controlle output
-
-
-	return controlled_out;
+void pidSMDSystem::setIntegratorLimit(double min_val, double max_val){
+	this->min_int_limit = min_val;
+	this->max_int_limit = max_val;
 }
 
+void pidSMDSystem::getIntegratorLimit(double &min_, double &max_){
+	max_ = this->max_int_limit;
+	min_ = this->min_int_limit;
+}
+
+void pidSMDSystem::setSystemLimits(double min_, double max_){
+	this->system_max = max_;
+	this->system_min = min_;
+}
+
+void pidSMDSystem::getSystemLimits(double &min_, double &max_){
+	min_ = this->system_min;
+	max_ = this->system_max;
+}
+
+double pidSMDSystem::pidUpdate(double set_pt, double measurement){
+
+	double controlled_out; //variable to return the controlle output
+
+	double error = set_pt - measurement; //error value
+
+	double proportional = this->kp * error; //proportinal part
+
+	//Integral
+    this->prev_integrator = this->prev_integrator + (this->ki * this->sampling_time * (error + this->prev_error))/2;
+
+	// Anti-wind-up via integrator clamping
+    if (this->prev_integrator > this->max_int_limit){
+
+        this->prev_integrator = this->max_int_limit;        
+    }
+    else if (this->prev_integrator < this->min_int_limit){
+
+        this->prev_integrator = this->min_int_limit;
+    }
+
+	//Derivative (band-limited prev_differentiator)
+    this->prev_differentiator = -(2.0f * this->kd * (measurement - this->prev_measurement)	
+                        + (2.0f * this->tau - this->sampling_time) * this->prev_differentiator)/ (2.0f * this->tau + this->sampling_time);
+
+    controlled_out = proportional + this->prev_integrator + this->prev_differentiator;
+
+    //clamping output vairable
+    if (controlled_out > this->system_max) {
+
+        controlled_out = this->system_max;
+
+    } else if (controlled_out < this->system_min) {
+
+        controlled_out = this->system_min;
+
+    }
+
+	// Store error and measurement for later use
+    this->prev_error = error;
+    this->prev_measurement = measurement;
+
+	// Return controller output
+	return controlled_out;
+}
 
 } //end of namespace
